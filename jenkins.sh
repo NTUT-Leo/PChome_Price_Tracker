3268b1b1a70fb164751894c6ed79826ac7e16da6
NTUT-Leo <t107590031@ntut.org.tw>
2023-06-25 04:21:50 +0800

    Modify "jenkins.sh" - big update.

--- jenkins.sh
+++ jenkins.sh
@@ -6,16 +6,25 @@ cd PChome_Price_Tracker
 pip install -r requirements.txt
 
 runTest(){
-    date
-    robot -F robot -d ./out/$BUILD_NUMBER -o output-1.xml -l NONE -r NONE -P ./"keywords" -L TRACE:INFO PChome\ Price\ Tracker.robot
-    ROUND=2
-    while [ $? -ne 0 ] && [ $ROUND -le $FAIL_RETRY ]; do
-        robot -F robot -d ./out/$BUILD_NUMBER -o output-$ROUND.xml -l NONE -r NONE -P ./"keywords" -L TRACE:INFO PChome\ Price\ Tracker.robot
+    ROUND=0
+    robot -F robot -d ./out/$BUILD_NUMBER -o output-0.xml -l NONE -r NONE -P ./"keywords" -L TRACE:INFO PChome\ Price\ Tracker.robot
+    while [[ $? -ne 0 && $ROUND -lt $FAIL_RETRY ]]; do
+        date
         ((ROUND++))
+        robot -F robot -d ./out/$BUILD_NUMBER -o output-$ROUND.xml -l NONE -r NONE -P ./"keywords" -L TRACE:INFO PChome\ Price\ Tracker.robot
     done
-    rebot -d ./out/$BUILD_NUMBER -l log.html -r report.html --merge --name output ./out/$BUILD_NUMBER/output-*.xml
+    date
+}
+
+postProcessing(){
+    echo =================================Test==Result=================================
+    rebot -d ./out/$BUILD_NUMBER/result -o output.xml -l log.html -r report.html --merge ./out/$BUILD_NUMBER/output-*.xml
+    echo ================================Merged==Output================================
+    rebot -d ./out/$BUILD_NUMBER -l log.html -r report.html --name PChome\ Price\ Tracker ./out/$BUILD_NUMBER/output-*.xml
+    rm -f ./out/$BUILD_NUMBER/output-*.xml
 }
 
 runTest
+postProcessing
 
 deactivate
